aa7caee286b4754348112b9b84d39400f2506bd4
Ben <benjisgarza@gmail.com>
2020-02-05 09:36:47 -0700

    Created showProjectsList function

---
+++ src/domManipulations.js
@@ -0,0 +1,18 @@
+const domManipulations = (() => {
+  const projects = ProjectModule.returnAllProjects();
+  const currentProject = 0;
+  const store = {};
+  const showProjectList = () => {
+    const projectListDiv = document.querySelector('#projectListDiv');
+    projectListDiv.innerHTML = '';
+    projects.forEach((project) => {
+      const childDiv = document.createElement('div');
+      childDiv.classList.add('project-item');
+      childDiv.innerHTML = project;
+      projectListDiv.insertAdjacentElement('beforeend', childDiv);
+      DomManipulations.clickOnProject();
+    });
+    const firstProjectOneList = document.querySelector('#projectListDiv > div:nth-child(1)');
+    firstProjectOneList.classList.add('currently_select_project');
+  };
+})()
