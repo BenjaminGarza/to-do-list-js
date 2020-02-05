18f140654f232ea1a24c4504266f475a378d301f
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-05 18:04:25 +0100

    created the creatProject function in the index.js file

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import DomManipulations from './DomManipulations'
+import ProjectModule from './ProjectModule'
+import Mixing from './Mixing'
+import todo from './todoFactory'
+import TodoSaved from './TodoSaved'
+
+const { projects } = DomManipulations
+
+const createProject = () => {
+  const projectButton = document.querySelector('#addProjectBtn')
+  projectButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    const projectInputField = document.querySelector('#projectInputField')
+    const projectName = document.querySelector('#projectInputField').value
+    if (projectName.length > 0) {
+      if (!projects.includes(projectName)) {
+        ProjectModule.addProject(projectName)
+        projectInputField.value = ''
+      } else {
+        alert('Project Already exists !!')
+      }
+    }
+    Mixing.closeTodoForm()
+    DomManipulations.showProjectList()
+    DomManipulations.displayTodoList()
+  })
+}
